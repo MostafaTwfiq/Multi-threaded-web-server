466d1089f50148ce65312a9b5d9bde239ca9e856
Mohamed Medhat Saeed Hamada Zayton <es-mohamed.medhat2019@alexu.edu.eg>
2022-04-29 17:38:43 +0200

    v1

--- web_server.py
+++ web_server.py
@@ -4,13 +4,16 @@ MY_HOST = "127.0.0.1"
 MY_PORT = 65432
 BUFFER_SIZE = 1024
 def server():
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((MY_HOST, MY_PORT))
-            s.listen()
-            conn, addr = s.accept()
-            conn_thread = threading.Thread(target=thread_fn, args=(conn, addr))
-            conn_thread.start()
+    try:
+        while True:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((MY_HOST, MY_PORT))
+                s.listen()
+                conn, addr = s.accept()
+                conn_thread = threading.Thread(target=thread_fn, args=(conn, addr))
+                conn_thread.start()
+    except KeyboardInterrupt:
+        print(1)
 
 
         
@@ -25,14 +28,22 @@ def thread_fn(conn, addr):
             result = parse_data(data=data)
             conn.sendall(data)
 
+# Parse GET and POST
 def parse_data(data):
     pass
 
-def store_file(file_name, file_data):
-    pass
 
+# Store File on POST Request
+def store_file(file_name, file_data):
+    with open(file_name, mode='w') as file: 
+        file.write(file_data)
+
+# Get file in GET Request
 def read_file(file_name):
-    pass
+    file_content = None
+    with open(file_name, mode='rb') as file: 
+        fileContent = file.read()
+    return file_content
 
 if __name__ == "__main__":
     server()
