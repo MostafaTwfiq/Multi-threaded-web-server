7680d66a755b9b5f87619366b1241ee335aa9ace
Mostafa Tawfiq <mostafa.mohammad.twfiq@gmail.com>
2022-05-02 21:40:24 +0200

    Implemented GET request part in client.

---
+++ main.py
@@ -0,0 +1,68 @@
+import json
+import email
+import pprint
+from io import StringIO
+
+import socket
+import sys
+import requests
+
+if __name__ == '__main__':
+
+    #r = requests.get('https://xkcd.com/1906/')
+    #print(r.json())
+    #print(r.headers)
+    response = requests.post(api, data=payload, headers=headers)
+    print(response)
+    request = "GET / HTTP/1.1\r\nHost:%s\r\n\r\nmostafa" % '127.0.0.1:80'
+    request = request.encode(encoding='UTF-8')
+
+    # Convert bytes to string type and string type to dict
+    string = request.decode('utf-8')
+    start_line, headers = string.split('\r\n', 1)
+    data = 'aa'
+    splitted = headers.split('\r\n')
+    #print(splitted)
+    for i, h in enumerate(splitted):
+        if h == '' and i < len(splitted) - 1:
+            data = splitted[i + 1]
+            break
+
+    #print(data)
+    #print()
+    #print(headers)
+
+    # construct a message from the request string
+    message = email.message_from_file(StringIO(headers.replace('\r\n\r\n', '\r\ndata:')))
+    print(message)
+
+    # construct a dictionary containing the headers
+    headers = dict(message.items())
+    print(headers)
+    # pretty-print the dictionary of headers
+    #pprint.pprint(headers, width=160)
+    #print(json_obj['source_name'])  # prints the string with 'source_name' key'''
+
+    '''
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Socket successfully created")
+    except socket.error as err:
+        print("socket creation failed with error %s" % (err))
+
+    # default port for socket
+    port = 80
+
+    try:
+        host_ip = socket.gethostbyname('www.google.com')
+        print(host_ip)
+    except socket.gaierror:
+
+        # this means could not resolve the host
+        print("there was an error resolving the host")
+        sys.exit()
+
+    # connecting to the server
+    s.connect((host_ip, port))
+
+    print("the socket has successfully connected to google")'''
--- web_client.py
+++ web_client.py
@@ -1,6 +1,80 @@
 from posixpath import split
 import socket
 from requests import head
+import os
+import sys
+
+HTTP_VERSION = "HTTP/1.1"
+SERVER_IP = "127.0.0.1"
+HTTP_PORT = 80
+BUFFER_SIZE = 2048
+
+def client(command_file):
+    commands = read_command(command_file)
+    for command in commands:
+        execute_command(generate_http_request(command))
+
+
+def read_command(command_file):
+    commands = None
+    with open(command_file, mode='w') as file:
+        commands = file.read().split('\r\n')
+
+    return commands
+
+
+def execute_command(command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((SERVER_IP, HTTP_PORT))
+        s.sendall(generate_http_request(command).encode(encoding='UTF-8'))
+        s.close()
+
+
+
+def generate_http_request(command):
+    request = ''
+    splitted_command = command.split(' ')
+    request = request + splitted_command[0] + splitted_command[1] + HTTP_VERSION + '\r\n'
+    request = request + "HOST: " + SERVER_IP + ":" + HTTP_PORT + '\r\n'
+
+    if splitted_command[0] == 'GET':
+        request = request + get_request_headers("GET") + '\r\n\r\n'
+    elif splitted_command[0] == 'POST':
+        data = read_file(splitted_command[1])
+        request = request + 'Content-Type: ' + ''
+        request = request + 'Content-Length:  ' + str(len(data))
+        request = request + get_request_headers("POST") + '\r\n\r\n'
+        request = request + data
+
+    return request
+
+def get_request_headers(method):
+    if method == "GET":
+        return ''
+    elif mathod == "POST":
+        return ''
+
+def get_content_type(file_path):
+    _, file_extension = os.path.splitext(file_path)
+    if file_extension == '.png':
+        return ContentType
+    elif file_extension == '.html':
+        return 'text/html'
+    elif file_extension == '.txt':
+        return 'text/plain'
+
+def read_file(file_name):
+    file_content = None
+    with open(file_name, mode='r') as file:
+        file_content = file.read()
+
+    return file_content
+
+if __name__ == "__main__":
+    client(sys.argv[0])
+
+
+
 
 dir_path = "/media/ahmad/New Volume/8th term/network/Ass/Multi-threaded-web-server/Data/"
 
@@ -13,7 +87,7 @@ def open_commands_file():
 #TAKE PARSED COMMAND AND CONNECT TO THE SERVER 
 #IF GET REQ. ONLY SEND HEADER AND SOME INFO 
 #IF POST REQ. EXTRA PART WILL BE SEND (MESSAGE BODY)
-def send_request(parsed_command , client):
+def send_request(parsed_command, client):
     host_number = parsed_command[2]
     port_number = parsed_command[3]
 
